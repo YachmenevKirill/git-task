9151acd258fd1d708c65dfe63c5efc89f6c43a61
Кирилл Ячменев <yachmenev-kv@yandex.ru>
2026-04-27 23:10:49 +0300

    fix bugs and add sqrt

--- calc.py
+++ calc.py
@@ -1,4 +1,6 @@
-# Автор: ...
+# Автор: Кирилл Ячменев
+
+import math
 
 def add(a, b):
     return a + b
@@ -9,9 +11,11 @@ def subtract(a, b):
 
 
 def multiply(a, b):
-    # TODO: не реализовано
-    return None
-
+    # TODO: выполнено
+    return a * b
+  
+def sqrt(x):
+    return math.sqrt(x)
 
 if __name__ == "__main__":
     print("Простой калькулятор запущен.")
